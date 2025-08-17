a257069d837b4e0af1a872cd150661df3436461f
Yesmi Liyanage <yesmi.liyanage@ascentic.se>
2025-08-17 19:25:40 +0530

    Fixing base url

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -3,7 +3,7 @@ import keycloak from "@/lib/keycloak";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: "https://web-app-event-management-backend.azurewebsites.net/api",
   headers: {
     Accept: "application/json",
     Authorization: `Bearer ${keycloak?.token}`,
